774f59ae4b53f7fd95a8a169399d8e3a7273f978
Willians-Hurtado <willianshurtado5@gmail.com>
2025-04-15 17:48:23 +0200

    Implement movie retrieval in index function

--- backend_api/controllers/MovieController.js
+++ backend_api/controllers/MovieController.js
@@ -2,7 +2,18 @@ const connection = require('../data/db.js');
 
 
 function index(req, res) {
-    res.json({ message: 'List of movies' });
+
+    const sql = 'SELECT * FROM movies';
+    connection.query(sql, (err, result) => {
+        if (err) return res.status(500).json({ error: err.message });
+
+        console.log(result);
+        res.json(result);
+
+
+
+    });
+
 
 };
 
